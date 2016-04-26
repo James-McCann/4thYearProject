bae56a2ae0fa01d71f6fd2d47830c3dc743cab5f
James McCann <jamesmccann16@gmail.com>
2016-04-26 17:43:54 +0100

    "Added image and removed alert"

--- client/templates/admin/vouchers/vouchers.js
+++ client/templates/admin/vouchers/vouchers.js
@@ -3,7 +3,7 @@ Template.searchBox.events({
     'submit .customer_rewards_form' : function(){
         var points = event.target.pickerPoints.value;
         var id = this._id.toString().substring(0,17);
-        alert('points: '+ points + ' '+ id);
+        //alert('points: '+ points + ' '+ id);
         Meteor.call('addPoints', id, points);
 
        return false;
